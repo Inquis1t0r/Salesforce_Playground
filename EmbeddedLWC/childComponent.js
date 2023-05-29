06e8dac11a4f9761ed25686384aa958110d24a49
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-05-29 12:52:49 +0200

    Create childComponent.js

---
+++ EmbeddedLWC/childComponent.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class ChildComponent extends LightningElement {
+    // Define the child component's functionality and properties
+    // Customize as needed
+}
